78b6b00d52fda7dd6845bb2a9a9a581e4b6778e5
Ifti Mustafa <i.mustafa@icloud.com>
2021-08-21 05:06:22 -0400

    Create Excel_automation.py

---
+++ Excel_automation.py
@@ -0,0 +1,28 @@
+# Working with Excel Spreadsheet, downloaded a sample xlsx file, 
+# modified data and added a graph. We took all prices from the
+# Excel sheet, and added a discount of 10% as a new price.
+
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = (cell.value * 0.9)
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+values = Reference(sheet,
+          min_row=2,
+          max_row=sheet.max_row,
+          min_col=4,
+          max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
+
+wb.save('transactions2.xlsx')
